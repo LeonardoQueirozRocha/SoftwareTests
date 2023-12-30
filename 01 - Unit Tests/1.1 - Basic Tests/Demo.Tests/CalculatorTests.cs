ab04d33b2405584e126a9991e4b7b0fcda3c1053
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-12-29 22:21:58 -0300

    test: Unit Test for Divide method

--- 01 - Unit Tests/1.1 - Basic Tests/Demo.Tests/CalculatorTests.cs
+++ 01 - Unit Tests/1.1 - Basic Tests/Demo.Tests/CalculatorTests.cs
@@ -33,4 +33,23 @@ public class CalculatorTests
         // Assert
         Assert.Equal(total, result);
     }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(2, 2, 1)]
+    [InlineData(4, 2, 2)]
+    [InlineData(7, 3, 2)]
+    [InlineData(6, 6, 1)]
+    [InlineData(9, 9, 1)]
+    public void Calculator_Divide_ReturnCorrectDividedValues(int v1, int v2, double total)
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Divide(v1, v2);
+
+        // Assert
+        Assert.Equal(total, result);
+    }
 }
