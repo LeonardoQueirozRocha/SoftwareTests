11f90d629b7eac50bd73800073ce8b394e50ac7b
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-12-30 22:21:38 -0300

    test: Using ordering feature to order test

---
+++ 01 - Unit Tests/2.2 - Features/Features.Tests/03 - Order/TestsOrder.cs
@@ -0,0 +1,54 @@
+﻿namespace Features.Tests.Order;
+
+[TestCaseOrderer("Features.Tests.Order.PriorityOrderer", "Features.Tests")]
+public class TestsOrder
+{
+    public static bool Test1Called;
+    public static bool Test2Called;
+    public static bool Test3Called;
+    public static bool Test4Called;
+
+    [Fact(DisplayName = "Test 01")]
+    [Trait("Category", "Tests Order"), TestPriority(2)]
+    public void Test1()
+    {
+        Test1Called = true;
+
+        Assert.True(Test3Called);
+        Assert.False(Test4Called);
+        Assert.False(Test2Called);
+    }
+
+    [Fact(DisplayName = "Test 02"), TestPriority(4)]
+    [Trait("Category", "Tests Order")]
+    public void Test02()
+    {
+        Test2Called = true;
+
+        Assert.True(Test3Called);
+        Assert.True(Test4Called);
+        Assert.True(Test1Called);
+    }
+
+    [Fact(DisplayName = "Test 03")]
+    [Trait("Category", "Tests Order"), TestPriority(1)]
+    public void Teste03()
+    {
+        Test3Called = true;
+
+        Assert.False(Test1Called);
+        Assert.False(Test2Called);
+        Assert.False(Test4Called);
+    }
+
+    [Fact(DisplayName = "Test 04"), TestPriority(3)]
+    [Trait("Category", "Tests Order")]
+    public void Test4()
+    {
+        Test4Called = true;
+
+        Assert.True(Test3Called);
+        Assert.True(Test1Called);
+        Assert.False(Test2Called);
+    }
+}
