b0bf2c583264426dc5c6d926bd1720fc6f29e24e
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2024-01-04 23:08:16 -0300

    tests: Unit Tests to order items

--- 02 - TDD/src/NerdStore.Sales.Domain/Models/Order.cs
+++ 02 - TDD/src/NerdStore.Sales.Domain/Models/Order.cs
@@ -1,19 +1,52 @@
+using NerdStore.Core.DomainObjects;
+using NerdStore.Sales.Domain.Enums;
+
 namespace NerdStore.Sales.Domain.Models;
 
 public class Order
 {
+    public static int MAX_UNITS_ITEM => 15;
+    public static int MIN_UNITS_ITEM => 1;
     private readonly List<OrderItem> _orderItems;
+
+    public Guid CustomerId { get; private set; }
     public decimal TotalValue { get; private set; }
+    public OrderStatus OrderStatus { get; private set; }
     public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
 
-    public Order()
+    protected Order()
     {
         _orderItems = new List<OrderItem>();
     }
 
+    public void MakeDraft() => OrderStatus = OrderStatus.Draft;
+
     public void AddItem(OrderItem orderItem)
     {
+        if (orderItem.Quantity > MAX_UNITS_ITEM) 
+            throw new DomainException($"Max of {MAX_UNITS_ITEM} units per products");
+
+        if (_orderItems.Any(o => o.ProductId == orderItem.ProductId))
+        {
+            var existingItem = _orderItems.FirstOrDefault(o => o.ProductId == orderItem.ProductId);
+            existingItem?.AddUnits(orderItem.Quantity);
+            orderItem = existingItem;
+            _orderItems.Remove(existingItem);
+        }
+
         _orderItems.Add(orderItem);
-        TotalValue = OrderItems.Sum(i => i.Quantity * i.UnitValue);
+        CalculateOrderValue();
+    }
+
+    private void CalculateOrderValue() => TotalValue = OrderItems.Sum(i => i.CalculateValue());
+
+    public static class OrderFactory
+    {
+        public static Order NewOrderDraft(Guid customerId)
+        {
+            var order = new Order { CustomerId = customerId };
+            order.MakeDraft();
+            return order;
+        }
     }
 }
