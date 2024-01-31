86c314e11e8c01a58b10f023a1832e0aab3fc8c3
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2024-01-31 10:05:16 -0300

    tests: Writing a integration test for register a user successfully

--- 03 - Integration Tests/tests/NerdStore.WebApp.Tests/UserTests.cs
+++ 03 - Integration Tests/tests/NerdStore.WebApp.Tests/UserTests.cs
@@ -1,8 +1,10 @@
 using NerdStore.WebApp.MVC;
 using NerdStore.WebApp.Tests.Configurations;
+using NerdStore.WebApp.Tests.Configurations.FixtureCollections;
 
 namespace NerdStore.WebApp.Tests;
 
+[Collection(nameof(IntegrationWebTestsFixtureCollection))]
 public class UserTests
 {
     private readonly IntegrationTestsFixture<Program> _testsFixture;
@@ -10,5 +12,39 @@ public class UserTests
     public UserTests(IntegrationTestsFixture<Program> testsFixture)
     {
         _testsFixture = testsFixture;
+    }
+
+    [Fact(DisplayName = "Accomplish register successfully")]
+    [Trait("Category", "Web Integration - User")]
+    public async Task User_AccomplishRegister_ShouldExecuteSuccessfully()
+    {
+        // Arrange
+        var requestUri = "/Identity/Account/Register";
+        var initialResponse = await _testsFixture.Client.GetAsync(requestUri);
+
+        initialResponse.EnsureSuccessStatusCode();
+
+        var email = "teste@teste.com";
+
+        var formData = new Dictionary<string, string>
+        {
+            { "Input.Email", email },
+            { "Input.Password", "Teste@123" },
+            { "Input.ConfirmPassword", "Teste@123" }
+        };
+
+        var postRequest = new HttpRequestMessage(HttpMethod.Post, requestUri)
+        {
+            Content = new FormUrlEncodedContent(formData)
+        };
+
+        // Act
+        var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+        // Assert
+        var responseString = await postResponse.Content.ReadAsStringAsync();
+
+        postResponse.EnsureSuccessStatusCode();
+        Assert.Contains($"Hello {email}!", responseString);
     }
 }
